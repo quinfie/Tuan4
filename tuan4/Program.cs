d94712b63d3ad4fa26ba0a844743805bbc41bd72
quinfie <dltquynh30@gmail.com>
2023-09-06 13:36:34 +0700

    first commit

---
+++ tuan4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tuan4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToaDo td = new ToaDo();
+            td.nhap();
+            td.xuat();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ tuan4/ToaDo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tuan4
+{
+    class ToaDo
+    {
+        private string ten;
+
+        public string Ten
+        {
+            get { return ten; }
+            set { ten = value; }
+        }
+        private float x;
+
+        public float X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        private float y;
+
+        public float Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public ToaDo()
+        {
+            this.x = 0;
+            this.y = 0;
+            this.ten = null;
+        }
+        public ToaDo(int x, int y, string ten)
+        {
+            this.x = x;
+            this.y = y;
+            this.ten = ten;
+        }
+        public void nhap()
+        {
+            Console.Write("Nhap ten toa do: ");
+            Ten = Console.ReadLine();
+            Console.Write("Nhap toa do x: ");
+            X = float.Parse(Console.ReadLine());
+            Console.Write("Nhap toa do y: ");
+            Y = float.Parse(Console.ReadLine());
+        }
+        public void xuat()
+        {
+            Console.Write("Toa do {0}({1},{2})", ten, x, y);
+        }
+    }
+}
